f61f3467e82b650f572f8081f067964c8d5e186d
Dániel Gulyás <daniel.gulyas@voiceworks.com>
2016-12-06 17:15:32 +0100

    Removed unnecessary boilerplate.

--- mix.exs
+++ mix.exs
@@ -7,26 +7,6 @@ defmodule Smile.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
-  end
-
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger]]
-  end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    []
+     deps: []]
   end
 end
