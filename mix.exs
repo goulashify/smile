ee4b10bf8f83729d0f128547bc07712921e4a40f
Dániel Gulyás <daniel.gulyas@voiceworks.com>
2016-12-06 17:28:38 +0100

    Updated readme, gitignore, deleted config, modified mix.exs to be able to pub.

--- mix.exs
+++ mix.exs
@@ -7,6 +7,28 @@ defmodule Smile.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: []]
+     deps: dependencies(),
+     description: description(),
+     package: package()]
+  end
+
+  defp dependencies do
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp description do
+    """
+      Small lib for converting emoji mappers to emoji characters, like in Slack messages, see GitHub for desc please.
+    """
+  end
+
+  defp package do
+    [
+      name: :smile,
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Daniel Gulyas"],
+      licenses: ["WTFPL"],
+      links: %{"GitHub" => "https://github.com/danigulyas/smile"}
+    ]
   end
 end
